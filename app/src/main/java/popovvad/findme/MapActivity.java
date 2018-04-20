731c43ab4f8195b9b67cebc2fd79ce4d9cd160cd
PopovVA <m.vadimpopov@gmail.com>
2018-04-20 14:35:05 +0300

    add integration yandex maps module

--- app/src/main/java/popovvad/findme/MapActivity.java
+++ app/src/main/java/popovvad/findme/MapActivity.java
@@ -1,13 +1,45 @@
 package popovvad.findme;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
 public class MapActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("0f8d1d87-de3b-4c52-823d-94463b58dae7");
+        MapKitFactory.initialize(this);
+
+        // Укажите имя activity вместо map.
         setContentView(R.layout.map_activity);
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
     }
 }
